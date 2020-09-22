724c363b0c688ed97d424279d27bb431e0538763
faraz <faraz.kashif5@gmail.com>
2020-09-22 15:42:48 +0500

    add text on about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = "https://api.darksky.net/forecast/e7ea57ca52da5ae872c7c0c343f312be/" + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
